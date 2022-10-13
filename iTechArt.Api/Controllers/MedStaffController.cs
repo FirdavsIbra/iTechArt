a295e1cb296db8381b42656906000dc171e013da
Ozodbek00 <kamoliddinovozodbek@mail.ru>
2022-10-13 21:33:00 +0500

    medStaff Service/Controller done

---
+++ iTechArt.Api/Controllers/MedStaffController.cs
@@ -0,0 +1,35 @@
+﻿using iTechArt.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iTechArt.Api.Controllers
+{
+    [ApiController, Route("api/medStaff")]
+    public class MedStaffController : ControllerBase
+    {
+        public readonly IMedStaffService medStaffService;
+
+        public MedStaffController(IMedStaffService medStaffService)
+        {
+            this.medStaffService = medStaffService;
+        }
+
+        [HttpPost("import")]
+        public IActionResult Import(IFormFile formFile)
+        {
+            if (formFile != null && (formFile.ContentType.Contains("csv") || formFile.ContentType.Contains("officedocument.spreadsheetml.sheet")))
+            {
+                return Ok(medStaffService.ImportMedStaffFile());
+            }
+            else
+            {
+                return BadRequest("Invalid file format!");
+            }
+        }
+
+        [HttpGet("export")]
+        public IActionResult Export()
+        {
+            return Ok(medStaffService.ExportMedStaffFile());
+        }
+    }
+}
