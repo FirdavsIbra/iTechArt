2f6fd6255f6aa402e543a4e49080ab8fa2614898
Nodir <kebridon.nemat.life@gmail.com>
2022-10-13 20:08:24 +0500

    Created initial structure for Student entity

---
+++ iTechArt.Service/Services/StudentService.cs
@@ -0,0 +1,23 @@
+﻿using iTechArt.Service.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArt.Service.Services
+{
+    public class StudentService : IStudentService
+    {
+        public IFormFile ExportStudent()
+        {
+            return new object() as IFormFile;
+        }
+
+        public void ImportStudent(IFormFile formFile)
+        {
+
+        }
+    }
+}
