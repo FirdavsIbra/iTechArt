be4063a10f6fe28813a02c0adffe896ab6c360a8
ShpilevskyAndrei <103843433+ShpilevskyAndrei@users.noreply.github.com>
2022-10-14 09:50:35 +0500

    [UI]
    created get requests on dashboard

--- iTechArt.UI/src/app/app.module.ts
+++ iTechArt.UI/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,13 @@ import { StatsModule } from './stats/stats.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, StatsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    StatsModule,
+    HttpClientModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- iTechArt.UI/src/app/shared/pages/dashboard-page/dashboard-page.component.ts
+++ iTechArt.UI/src/app/shared/pages/dashboard-page/dashboard-page.component.ts
@@ -1,12 +1,122 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { UNITS } from "../../constants/units";
+import { UNITS } from '../../constants/units';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss'],
 })
-export class DashboardPageComponent {
-  public units: typeof UNITS = UNITS
+export class DashboardPageComponent implements OnInit {
+  public units: typeof UNITS = UNITS;
+  public url = environment.apiUrl;
+
+  public count = {
+    policeCount: '',
+    medStaffCount: '',
+    groceryCount: '',
+    pupilsCount: '',
+    studentsCount: '',
+    airportCount: '',
+  };
+
+  public apis = {
+    police: {
+      import: 'api/police/import',
+      export: 'api/police/export',
+    },
+    medStaff: {
+      import: 'api/medStaff/import',
+      export: 'api/medStaff/export',
+    },
+    grocery: {
+      import: 'api/grocery/import',
+      export: 'api/grocery/export',
+    },
+    pupils: {
+      import: 'api/pupils/import',
+      export: 'api/pupils/export',
+    },
+    students: {
+      import: 'Students/import',
+      export: 'Students/export',
+    },
+    airport: {
+      import: 'Airport/import',
+      export: 'Airport/export',
+    },
+  };
+
+  public constructor(private http: HttpClient) {}
+
+  public ngOnInit(): void {
+    this.getAirport();
+    this.getPolice();
+    this.getPupils();
+    this.getGrocery();
+    this.getMedStaff();
+    this.getStudents();
+  }
+
+  public getAirport(): void {
+    this.http
+      .get<any>(`${this.url}${this.apis.students.export}`)
+      .subscribe((data) => {
+        this.count.airportCount = data.total;
+      });
+
+    console.log(this.count.airportCount);
+  }
+
+  public getPolice(): void {
+    this.http
+      .get<any>(`${this.url}${this.apis.police.export}`)
+      .subscribe((data) => {
+        this.count.policeCount = data.total;
+      });
+
+    console.log(this.count.policeCount);
+  }
+
+  public getMedStaff(): void {
+    this.http
+      .get<any>(`${this.url}${this.apis.medStaff.export}`)
+      .subscribe((data) => {
+        this.count.medStaffCount = data.total;
+      });
+
+    console.log(this.count.medStaffCount);
+  }
+
+  public getGrocery(): void {
+    this.http
+      .get<any>(`${this.url}${this.apis.grocery.export}`)
+      .subscribe((data) => {
+        this.count.groceryCount = data.total;
+      });
+
+    console.log(this.count.groceryCount);
+  }
+
+  public getStudents(): void {
+    this.http
+      .get<any>(`${this.url}${this.apis.students.export}`)
+      .subscribe((data) => {
+        this.count.studentsCount = data.total;
+      });
+
+    console.log(this.count.studentsCount);
+  }
+
+  public getPupils(): void {
+    this.http
+      .get<any>(`${this.url}${this.apis.pupils.export}`)
+      .subscribe((data) => {
+        this.count.pupilsCount = data.total;
+      });
+
+    console.log(this.count.pupilsCount);
+  }
 }
