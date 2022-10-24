09f1ba7187547fec4e65674f80fdeb81c83b15ec
DilshodMura <75676999+DilshodMura@users.noreply.github.com>
2022-10-24 04:47:10 -0500

    grocery controller changes

--- iTechArt.Api/Controllers/GroceryController.cs
+++ iTechArt.Api/Controllers/GroceryController.cs
@@ -8,15 +8,20 @@ namespace iTechArt.Api.Controllers
     public sealed class GroceryController : Controller
     {
         private readonly IGroceryService _groceryService;
+        private readonly string[] fileExtensions = { ".xlsx", ".xls", ".csv", "application/vnd.ms-excel", "officedocument.spreadsheetml.sheet" };
+
         public GroceryController(IGroceryService groceryService)
         {
             _groceryService = groceryService;
         }
-
+        /// <summary>
+        /// api route which applies the following extensions
+        /// will allow to upload the data from file to db 
+        /// </summary>
         [HttpPost(ApiConstants.IMPORT)]
         public IActionResult Import(IFormFile formFile)
         {
-            if (formFile != null && (formFile.ContentType.Contains("application/vnd.ms-excel") || formFile.ContentType.Contains("officedocument.spreadsheetml.sheet")))
+            if (formFile != null && (fileExtensions.Any(fileExtensions => !formFile.ContentType.Contains(fileExtensions))))
             {
                 return Ok(_groceryService.ImportGrocery());
             }
@@ -25,7 +30,9 @@ namespace iTechArt.Api.Controllers
                 return BadRequest("Invalid file format!");
             }
         }
-
+        /// <summary>
+        /// api route which allows to get all info from db and parse it to the following format
+        /// </summary>
         [HttpGet(ApiConstants.EXPORT)]
         public IActionResult Export()
         {
