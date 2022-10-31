ca7deeb31af58b58322f890fa3801b9f4bc86895
Ozodbek00 <kamoliddinovozodbek@mail.ru>
2022-10-31 14:57:32 +0500

    medStaff xml parser

--- iTechArt.Service/Services/MedStaffService.cs
+++ iTechArt.Service/Services/MedStaffService.cs
@@ -1,10 +1,15 @@
-﻿using iTechArt.Domain.Enums;
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using iTechArt.Domain.Enums;
 using iTechArt.Domain.ModelInterfaces;
 using iTechArt.Domain.RepositoryInterfaces;
 using iTechArt.Domain.ServiceInterfaces;
 using iTechArt.Service.DTOs;
+using iTechArt.Service.Helpers;
 using Microsoft.AspNetCore.Http;
 using OfficeOpenXml;
+using System.Globalization;
+using System.Xml;
 
 namespace iTechArt.Service.Services
 {
@@ -35,6 +40,14 @@ namespace iTechArt.Service.Services
             if (fileExtension == ".xlsx")
             {
                 await ExcelParser(file);
+            }
+            else if (fileExtension == ".csv")
+            {
+                await CsvParser(file);
+            }
+            else if (fileExtension == ".xml")
+            {
+                await XmlParser(file);
             }
         }
 
@@ -76,6 +89,91 @@ namespace iTechArt.Service.Services
             catch (Exception ex)
             {
                 throw new ArgumentException(nameof(ex));
+            }
+        }
+
+        private async Task CsvParser(IFormFile file)
+        {
+            var filePath = Path.Combine(EnvironmentHelper.AttachmentPath, file.FileName);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+            var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ",",
+                HasHeaderRecord = true
+            };
+
+            using (StreamReader csvReader = new StreamReader(filePath))
+            {
+                using CsvReader csv = new(csvReader, configuration);
+                
+                var records = csv.GetRecords<MedStaffDTO>().ToList();
+
+                foreach (var record in records)
+                {
+                    MedStaffDTO medStaff = new MedStaffDTO
+                    {
+                        FirstName = record.FirstName,
+                        LastName = record.LastName,
+                        Gender = record.Gender,
+                        Email = record.Email,
+                        PhoneNumber = record.PhoneNumber,
+                        DateOfBirth = record.DateOfBirth,
+                        Address = record.Address,
+                        Salary = record.Salary,
+                        HospitalName = record.HospitalName,
+                        PostalCode = record.PostalCode,
+                        Shift = record.Shift
+                    };
+
+                    await _medStaffRepository.AddAsync(medStaff);
+                }
+            }
+            
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        private async Task XmlParser(IFormFile file)
+        {
+            var filePath = Path.Combine(EnvironmentHelper.AttachmentPath, file.FileName);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(filePath);
+            
+            foreach (XmlNode node in xmlDocument.SelectNodes("/dataset/record"))
+            {
+                MedStaffDTO medStaff = new MedStaffDTO
+                {
+                    FirstName = node["FirstName"].InnerText,
+                    LastName = node["LastName"].InnerText,
+                    Gender = (Gender)Convert.ToByte(node["Gender"].InnerText),
+                    Email = node["Email"].InnerText,
+                    PhoneNumber = node["PhoneNumber"].InnerText,
+                    DateOfBirth = Convert.ToDateTime(node["DateOfBirth"].InnerText),
+                    Address = node["Address"].InnerText,
+                    Salary = Convert.ToDecimal(node["Salary"].InnerText),
+                    HospitalName = node["HospitalName"].InnerText,
+                    PostalCode = node["PostalCode"] is null? String.Empty: node["PostalCode"].InnerText,
+                    Shift = (Shift)Convert.ToByte(node["Shift"].InnerText)
+                };
+
+                await _medStaffRepository.AddAsync(medStaff);
+            }
+            
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
             }
         }
     }
