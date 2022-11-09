cfb8f065096c0403c5c3ec3570bbda260805bf0b
Firdavs <103358030+FirdavsIbra@users.noreply.github.com>
2022-11-09 16:01:43 +0500

    DateTime to DateOnly migration in Airport

---
+++ iTechArt.Database/Migrations/20221109105915_DateOnlyAirport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace iTechArt.Database.Migrations
+{
+    public partial class DateOnlyAirport : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "BuiltDate",
+                table: "Airports",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BuiltDate",
+                table: "Airports",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
