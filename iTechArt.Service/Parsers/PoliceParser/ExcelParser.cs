efc073b620c1aafcd0aa683212865dbae332a4d9
Khasan <k.kholmatov@student.inha.uz>
2022-11-09 18:05:34 +0500

    getall route changed

--- iTechArt.Service/Parsers/PoliceParser/ExcelParser.cs
+++ iTechArt.Service/Parsers/PoliceParser/ExcelParser.cs
@@ -5,6 +5,7 @@ using iTechArt.Domain.RepositoryInterfaces;
 using iTechArt.Service.DTOs;
 using Microsoft.AspNetCore.Http;
 using OfficeOpenXml;
+using Spire.Xls;
 
 namespace iTechArt.Service.Parsers.PoliceParser
 {
@@ -25,30 +26,42 @@ namespace iTechArt.Service.Parsers.PoliceParser
             string fileExtension = Path.GetExtension(file.FileName);
             var polices = new List<IPolice>();
 
-            using (var fileStream = new MemoryStream())
+            if (fileExtension == ".xlsx")
             {
-                await file.CopyToAsync(fileStream);
-                using (var package = new ExcelPackage(fileStream))
+                using (var fileStream = new MemoryStream())
                 {
-                    ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-
-                    var rowCount = worksheet.Dimension.Rows;
-                    for (int row = 2; row <= rowCount; row++)
+                    await file.CopyToAsync(fileStream);
+                    using (var package = new ExcelPackage(fileStream))
                     {
-                        var policeDto = new PoliceDto
+                        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                        ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+                        var rowCount = worksheet.Dimension.Rows;
+                        for (int row = 2; row <= rowCount; row++)
                         {
-                            Name = worksheet.Cells[row, 1].Value.ToString().Trim(),
-                            Surname = worksheet.Cells[row, 2].Value.ToString().Trim(),
-                            Email = worksheet.Cells[row, 3].Value.ToString().Trim(),
-                            Gender = Enum.Parse<Gender>(worksheet.Cells[row, 4].Value.ToString()),
-                            Address = worksheet.Cells[row, 5].Value.ToString().Trim(),
-                            JobTitle = worksheet.Cells[row, 6].Value.ToString().Trim(),
-                            Salary = Convert.ToDouble(worksheet.Cells[row, 7].Value)
-                        };
-                        polices.Add(policeDto);
+                            var policeDto = new PoliceDto
+                            {
+                                Name = worksheet.Cells[row, 1].Value.ToString().Trim(),
+                                Surname = worksheet.Cells[row, 2].Value.ToString().Trim(),
+                                Email = worksheet.Cells[row, 3].Value.ToString().Trim(),
+                                Gender = Enum.Parse<Gender>(worksheet.Cells[row, 4].Value.ToString()),
+                                Address = worksheet.Cells[row, 5].Value.ToString().Trim(),
+                                JobTitle = worksheet.Cells[row, 6].Value.ToString().Trim(),
+                                Salary = Convert.ToDouble(worksheet.Cells[row, 7].Value)
+                            };
+                            polices.Add(policeDto);
+                        }
+                        await _policeRepository.AddRangeAsync(polices.ToArray());
                     }
-                    await _policeRepository.AddRangeAsync(polices.ToArray());
+                }
+            }
+            else
+            {
+                using (var fileStream = new MemoryStream())
+                {
+                    await file.CopyToAsync(fileStream);
+                    Workbook workbook = new Workbook();
+                    Worksheet worksheet = workbook.Worksheets[0];
                 }
             }
         }
