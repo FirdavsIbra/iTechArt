73c7cd3a7102f2c61f0b590bd74ff936718323dd
Albina <a.pavlova@student.inha.uz>
2022-11-09 18:39:54 +0500

    [UI] Quick fixes 2.0(((

--- iTechArt.UI/src/app/stats/modules/police/pages/police-page/police-page.component.ts
+++ iTechArt.UI/src/app/stats/modules/police/pages/police-page/police-page.component.ts
@@ -14,21 +14,20 @@ export class PolicePageComponent implements OnInit {
   public data: UnitsTypes | undefined;
 
   public columns = [
-    { field: 'id', header: 'Id', width: 41 },
-    { field: 'firstName', header: 'First Name', width: 200 },
-    { field: 'lastName', header: 'Last Name', width: 100 },
-    { field: 'gender', header: 'Gender', width: 100 },
-    { field: 'email', header: 'Email', width:100 },
-    { field: 'address', header: 'Address', width:100 },
-    { field: 'jobTitle', header: 'Job Title', width:100 }
+    { field: 'id', header: 'Id', width: 58 },
+    { field: 'name', header: 'First Name', width: 217 },
+    { field: 'surname', header: 'Last Name', width: 117 },
+    { field: 'gender', header: 'Gender', width: 117 },
+    { field: 'email', header: 'Email', width: 261 },
+    { field: 'address', header: 'Address', width: 117 },
+    { field: 'jobTitle', header: 'Job Title', width: 117 },
   ];
-
 
   public constructor(private statsService: StatsService) {}
 
   public ngOnInit(): void {
     this.statsService.getAllStatsByUnit(this.unit).subscribe({
-      next: (data: UnitsTypes) => this.data = data,
+      next: (data: UnitsTypes) => (this.data = data),
       error: () => alert("Couldn't load data."),
     });
   }
